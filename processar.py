4a2beb20bf789aca6db46b863b9b2e762643a415
Luciano Ramalho <luciano@ramalho.org>
2013-06-16 20:04:29 -0300

    retoque automatico de margens internas brancas

--- processar.py
+++ processar.py
@@ -13,12 +13,26 @@ import glob
 import re
 from PIL import Image
 
-PATH = 'testes/'
+#PATH = 'testes/'
 PATH = 'scans/'
 PADRAO_PAGINA = re.compile(r'p\d\d\d\.png')
 SUFIXO_VERSAO = re.compile(r'.*-(\d+)')
 DIM_ORIG = (4091, 2962)
 DIM_CROP = (3800, 2900)
+MARGENS = [
+    (1775,    0, 1900, 1450),
+    (1775, 1450, 1900, 2900),
+    (1900,    0, 2025, 1450),
+    (1900, 1450, 2025, 2900),
+]
+AREAS_AMOSTRAS = [
+    (1775,  200, 1825, 1400),
+    (1775, 1500, 1825, 2700),
+    (1950,  200, 2000, 1400),
+    (1950, 1500, 2000, 2700),
+]
+DIM_1000 = (1006, 768)
+DIM_300 =   (314, 240)
 
 def extrair_num_copia(nome_orig):
     """
@@ -72,15 +86,55 @@ def cortar(nome_img):
     with open(nome_img, 'wb') as saida:
         im_crop.save(saida, 'PNG')
 
+
+def media_rgb(amostra):
+    somas = (sum(a[0] for a in amostra),
+             sum(a[1] for a in amostra),
+             sum(a[2] for a in amostra))
+    return (somas[0]/len(amostra), somas[1]/len(amostra), somas[2]/len(amostra))
+
+def mediana(amostra):
+    return sorted(amostra)[len(amostra)/2]
+
+def retocar_margens_internas(nome_img):
+    with open(nome_img) as entrada:
+        im = Image.open(entrada)
+        pix = im.load()
+        a_pintar = []
+        im_retoque = None
+        for area in AREAS_AMOSTRAS:
+            amostra = []
+            for x in range(area[0], area[2], 10):
+                for y in range(area[1], area[3], 10):
+                    amostra.append(pix[x, y])
+            valores = media_rgb(amostra) + mediana(amostra)
+            dif = 255*6 - sum(valores)
+            if dif < 20:
+                a_pintar.append(area)
+                if im_retoque is None:
+                    im_retoque = im.copy()
+            else:
+                a_pintar.append(None)
+
+    if im_retoque:
+        backup(nome_img)
+        with open(nome_img, 'wb') as saida:
+            for pintar, area in zip(a_pintar, MARGENS):
+                if pintar:
+                    im_retoque.paste((255, 255, 255), area)
+            im_retoque.save(saida, 'PNG')
+
+
 def main():
     for dir_path, dir_names, nomes_arqs in os.walk(PATH):
         for nome_img in nomes_arqs:
             if PADRAO_PAGINA.match(nome_img):
                 path_img = os.path.join(dir_path, nome_img)
                 t0 = time.time()
-                backup(path_img)
-                rotacionar(path_img)
-                cortar(path_img)
+                #backup(path_img)
+                #rotacionar(path_img)
+                #cortar(path_img)
+                retocar_margens_internas(path_img)
                 print '%0.3fs %s' % (time.time()-t0, path_img)
 
 if __name__=='__main__':
