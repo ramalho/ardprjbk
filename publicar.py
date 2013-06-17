f4176bd1c95178b5111aa76271532390f2f8e1aa
Luciano Ramalho <luciano@ramalho.org>
2013-06-16 23:22:13 -0300

    geracao de paginas intermediarias com thumb de 1000x700

--- publicar.py
+++ publicar.py
@@ -4,32 +4,44 @@
 """
 Script descartável para publicar scans de um livro.
 """
-
-HTML = """
-<!doctype html>
-<html>
-<head>
-    <title>Arduino Projects Book</title>
-</head>
-<body>
-<h1>Arduino Projects Book</h1>
-
-Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License by Arduino LLC
-(<a href="scans/front/p002.png">license page</a>)
-
-{table}
-
-</body>
-</html>
-"""
-
 import os
+import io
 import re
 import pprint
 import itertools
 
+HTML5 = """
+<!doctype html>
+<html>
+<head>
+    <title>{title}</title>
+</head>
+<body>
+{body}
+</body>
+</html>
+"""
+
+BODY_IDX = """
+<h1>Arduino Projects Book</h1>
+
+<p>Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License by Arduino LLC
+(<a href="scans/front/p002.png">license page</a>)</p>
+
+{table}
+"""
+
+HTML_IDX = HTML5.format(title='Arduino Projects Book', body=BODY_IDX)
+
+BODY_PG = ("""<a href="{link}">"""
+           """<img src="{img}" title="pages {pg}-{pg1} (click for high resolution scan)" />"""
+           """</a>""")
+
+HTML_PG = HTML5.format(title='Arduino Projects Book: p.{pg}-{pg1}', body=BODY_PG)
+
 PATH = 'scans/'
-PATH_THUMBS = 'scans/thumbs/'
+PATH_THUMBS = 'thumbs/'
+PATH_PAGINAS = 'pages/'
 PADRAO_PAGINA = re.compile(r'p(\d\d\d)\.png')
 DIM_THUMBS = {'1000'  : (1006, 768),
               '300' : (314, 239)}
@@ -57,7 +69,7 @@ def gerar_tabela(indice, max_paginas):
         #print '\t<tr><th>{}</th></tr>'.format(chave_cap)
         tab.append('\t<tr>')
         for pg in paginas:
-            link = chave_cap + '/p{:03d}.png'.format(pg)
+            link = PATH_PAGINAS + 'p{:03d}.html'.format(pg)
             img = PATH_THUMBS + '300/p{:03d}.png'.format(pg)
             larg = DIM_THUMBS['300'][0]
             alt = DIM_THUMBS['300'][1]
@@ -67,11 +79,22 @@ def gerar_tabela(indice, max_paginas):
                    '</a></td>'.format(**locals()))
         tab.append('\t</tr>')
     tab.append('</table>')
-    return HTML.format(table='\n'.join(tab))
+    return HTML_IDX.format(table='\n'.join(tab))
+
+def gerar_paginas(indice):
+    for chave_cap, paginas in sorted(indice.items()):
+        for pg in paginas:
+            with io.open(PATH_PAGINAS+'p{:03d}.html'.format(pg), 'w', encoding='utf-8') as saida:
+                link = '../' + chave_cap + '/p{:03d}.png'.format(pg)
+                img = '../' + PATH_THUMBS + '1000/p{:03d}.png'.format(pg)
+                html = HTML_PG.format(pg=pg, pg1=pg+1, link=link, img=img)
+                saida.write(html.decode('utf-8'))
+
 
 def main():
     indice, max_paginas = indexar()
     print gerar_tabela(indice, max_paginas)
+    gerar_paginas(indice)
 
 if __name__=='__main__':
     main()
